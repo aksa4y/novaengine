f37141f16223bfc1643317bbe0bba928ed719438
aksa4y <76872734+aksa4y@users.noreply.github.com>
2026-09-06 01:48:24 +0300

    feat(rhi): add API-neutral device interface

---
+++ rhi/include/nova/rhi/RHI.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <cstdint>
+#include <memory>
+#include <string_view>
+
+namespace Nova::RHI {
+
+enum class Backend : std::uint8_t {
+    Null,
+    Sokol,
+    Vulkan,
+    D3D11,
+    D3D12,
+    Metal,
+    OpenGL,
+    WebGPU,
+};
+
+struct DeviceDesc {
+    Backend backend = Backend::Null;
+    bool debug = false;
+};
+
+struct TextureDesc {
+    std::uint32_t width = 1;
+    std::uint32_t height = 1;
+    std::uint32_t mip_levels = 1;
+};
+
+class Texture {
+public:
+    virtual ~Texture() = default;
+};
+
+class Device {
+public:
+    virtual ~Device() = default;
+
+    virtual Backend backend() const noexcept = 0;
+    virtual std::string_view backend_name() const noexcept = 0;
+
+    virtual std::unique_ptr<Texture> create_texture(const TextureDesc& desc) = 0;
+    virtual void begin_frame() = 0;
+    virtual void end_frame() = 0;
+};
+
+std::unique_ptr<Device> create_device(const DeviceDesc& desc = {});
+
+} // namespace Nova::RHI
